021919af46f0c71fe7d823fdd99310d1fb498992
dengjie <1030342126@qq.com>
2023-11-28 21:58:20 +0800

    fix(lab2 raft.go): 修改raft.go中apply逻辑，使得一次apply结束前不会启动下一次apply

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,19 @@
 package kvraft
 
-import "6.824/labrpc"
+import (
+	"6.824/labrpc"
+	"sync/atomic"
+)
 import "crypto/rand"
 import "math/big"
-
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	leader  int
+	peerNum int
+	clerkID int64
+	seqID   int64
 }
 
 func nrand() int64 {
@@ -21,10 +27,13 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.clerkID = nrand()
+	ck.seqID = 0
+	ck.leader = 0
+	ck.peerNum = len(ck.servers)
 	return ck
 }
 
-//
 // fetch the current value for a key.
 // returns "" if the key does not exist.
 // keeps trying forever in the face of all other errors.
@@ -35,14 +44,32 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+	args := GetArgs{
+		Key:     key,
+		ClerkID: ck.clerkID,
+		SeqID:   atomic.AddInt64(&ck.seqID, 1),
+	}
+	for {
+		reply := GetReply{}
+		ok := ck.servers[ck.leader].Call("KVServer.Get", &args, &reply)
+		if !ok {
+			// 轮询
+			ck.leader = (ck.leader + 1) % len(ck.servers)
+		} else {
+			if reply.Err == "NOT LEADER" {
+				ck.leader = (ck.leader + 1) % len(ck.servers)
+			}
+			if reply.Err == "OK" {
+				return reply.Value
+			}
+		}
+	}
+
 }
 
-//
 // shared by Put and Append.
 //
 // you can send an RPC with code like this:
@@ -51,14 +78,38 @@ func (ck *Clerk) Get(key string) string {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	args := PutAppendArgs{
+		Key:     key,
+		Value:   value,
+		Op:      op,
+		ClerkID: ck.clerkID,
+		SeqID:   atomic.AddInt64(&ck.seqID, 1),
+	}
+	for {
+		reply := PutAppendReply{}
+		ok := ck.servers[ck.leader].Call("KVServer.PutAppend", &args, &reply)
+		if !ok {
+			// 轮询
+			ck.leader = (ck.leader + 1) % len(ck.servers)
+		} else {
+			if reply.Err == "NOT LEADER" {
+				DPrintf("WARN %d NOT LEADER", ck.leader)
+				ck.leader = (ck.leader + 1) % len(ck.servers)
+			}
+			if reply.Err == "OK" {
+				return
+			}
+		}
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
+	DPrintf("Client Put (%s, %s)", key, value)
 	ck.PutAppend(key, value, "Put")
 }
 func (ck *Clerk) Append(key string, value string) {
+	DPrintf("Client Append (%s, %s)", key, value)
 	ck.PutAppend(key, value, "Append")
 }
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -7,9 +7,18 @@ import (
 	"log"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 const Debug = false
+
+const (
+	ErrNotLeader = "NOT LEADER"
+	ErrOK        = "OK"
+	GetOp        = "Get"
+	PutOp        = "Put"
+	AppendOp     = "Append"
+)
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
@@ -18,11 +27,16 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	OpType  string
+	Key     string
+	Value   string
+	ClerkID int64
+	SeqID   int64
+	Index   int // raft服务层传回来的Index
 }
 
 type KVServer struct {
@@ -35,18 +49,115 @@ type KVServer struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	SeqMap    map[int64]int64
+	waitChMap map[int]chan Op
+	KVData    map[string]string
 }
 
+func (kv *KVServer) GetWaitCh(LogIndex int) chan Op {
+	ch, exist := kv.waitChMap[LogIndex]
+	if !exist {
+		kv.waitChMap[LogIndex] = make(chan Op, 1)
+		ch = kv.waitChMap[LogIndex]
+	}
+	return ch
+}
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	if kv.killed() {
+		reply.Err = ErrNotLeader
+		return
+	}
+
+	op := Op{
+		OpType:  GetOp,
+		Key:     args.Key,
+		ClerkID: args.ClerkID,
+		SeqID:   args.SeqID,
+	}
+
+	LogIndex, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.Err = ErrNotLeader
+		return
+	}
+	kv.mu.Lock()
+	ch := kv.GetWaitCh(LogIndex)
+	kv.mu.Unlock()
+	defer func() {
+		kv.mu.Lock()
+		delete(kv.waitChMap, op.Index)
+		kv.mu.Unlock()
+	}()
+	timer := time.NewTimer(150 * time.Millisecond)
+
+	select {
+	case <-timer.C:
+		reply.Err = ErrNotLeader
+		return
+	case replyOp := <-ch:
+		if replyOp.ClerkID != op.ClerkID || replyOp.SeqID != op.SeqID {
+			reply.Err = ErrNotLeader
+			return
+		}
+		reply.Err = ErrOK
+		kv.mu.Lock()
+		reply.Value = kv.KVData[args.Key]
+		kv.mu.Unlock()
+		return
+	}
+
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	if kv.killed() {
+		reply.Err = ErrNotLeader
+		return
+	}
+
+	op := Op{
+		OpType:  args.Op,
+		Key:     args.Key,
+		Value:   args.Value,
+		ClerkID: args.ClerkID,
+		SeqID:   args.SeqID,
+	}
+
+	LogIndex, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.Err = ErrNotLeader
+		return
+	}
+
+	kv.mu.Lock()
+	ch := kv.GetWaitCh(LogIndex)
+	kv.mu.Unlock()
+
+	defer func() {
+		kv.mu.Lock()
+		delete(kv.waitChMap, op.Index)
+		kv.mu.Unlock()
+	}()
+
+	timer := time.NewTimer(100 * time.Millisecond)
+
+	select {
+	case replyOp := <-ch:
+		if op.ClerkID != replyOp.ClerkID || op.SeqID != replyOp.SeqID {
+			reply.Err = ErrNotLeader
+			return
+		}
+		reply.Err = ErrOK
+		return
+	case <-timer.C:
+		reply.Err = ErrNotLeader
+		return
+	}
+
 }
 
-//
 // the tester calls Kill() when a KVServer instance won't
 // be needed again. for your convenience, we supply
 // code to set rf.dead (without needing a lock),
@@ -55,7 +166,6 @@ func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 // code to Kill(). you're not required to do anything
 // about this, but it may be convenient (for example)
 // to suppress debug output from a Kill()ed instance.
-//
 func (kv *KVServer) Kill() {
 	atomic.StoreInt32(&kv.dead, 1)
 	kv.rf.Kill()
@@ -67,7 +177,44 @@ func (kv *KVServer) killed() bool {
 	return z == 1
 }
 
-//
+func (kv *KVServer) ifDuplicate(clerkID int64, seqID int64) bool {
+	lastSeqIndex, exist := kv.SeqMap[clerkID]
+	if !exist {
+		return false
+	}
+	return lastSeqIndex >= seqID
+}
+
+func (kv *KVServer) applyMsgHandlerLoop() {
+	for {
+		if kv.killed() {
+			return
+		}
+		select {
+		case msg := <-kv.applyCh:
+			if msg.CommandValid {
+				kv.mu.Lock()
+				op := msg.Command.(Op)
+				LogIndex := msg.CommandIndex
+				if !kv.ifDuplicate(op.ClerkID, op.SeqID) {
+					if op.OpType == PutOp {
+						kv.KVData[op.Key] = op.Value
+					}
+					if op.OpType == AppendOp {
+						DPrintf("Before (%s, %s) Append (%s, %s)", op.Key, op.Value, op.Key, kv.KVData[op.Key])
+						kv.KVData[op.Key] += op.Value
+						DPrintf("After (%s, %s) Append (%s, %s)", op.Key, op.Value, op.Key, kv.KVData[op.Key])
+					}
+					kv.SeqMap[op.ClerkID] = op.SeqID
+				}
+				op.Index = msg.CommandIndex
+				kv.GetWaitCh(LogIndex) <- op
+				kv.mu.Unlock()
+			}
+		}
+	}
+}
+
 // servers[] contains the ports of the set of
 // servers that will cooperate via Raft to
 // form the fault-tolerant key/value service.
@@ -80,7 +227,6 @@ func (kv *KVServer) killed() bool {
 // you don't need to snapshot.
 // StartKVServer() must return quickly, so it should start goroutines
 // for any long-running work.
-//
 func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister, maxraftstate int) *KVServer {
 	// call labgob.Register on structures you want
 	// Go's RPC library to marshall/unmarshall.
@@ -96,6 +242,11 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	kv.SeqMap = make(map[int64]int64)
+	kv.KVData = make(map[string]string)
+	kv.waitChMap = make(map[int]chan Op)
+
+	go kv.applyMsgHandlerLoop()
 
 	return kv
 }
